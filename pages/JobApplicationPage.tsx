1c7373fca45a2c6b02e3ca50041e579243f4d18b
zach rizzo <zachcilwa@gmail.com>
2022-06-27 14:37:11 -0700

    inital beta build

--- pages/JobApplicationPage.tsx
+++ pages/JobApplicationPage.tsx
@@ -788,7 +788,7 @@ const JobApplicationPage: NextPage<{}> = () => {
             <h4 className=" my-10 text-center text-3xl text-[#4e4e4e]">
               Education
             </h4>
-            <EducationBox
+            {/* <EducationBox
               highSchoolState={setHighSchoolState}
               highSchoolValue={highSchoolState}
               highSchoolCourseOfStudyState={setHighSchoolCourseOfStudyState}
@@ -955,7 +955,7 @@ const JobApplicationPage: NextPage<{}> = () => {
               ReferenceCompanyState4={setReferenceCompanyState4}
               ReferencePhoneNumberValue4={referencePhoneNumberState4}
               ReferencePhoneNumberState4={setReferencePhoneNumberState4}
-            />
+            /> */}
             {/* <h4 className=" my-10 text-center text-3xl text-[#4e4e4e]">
               Acknowledgment
             </h4>
