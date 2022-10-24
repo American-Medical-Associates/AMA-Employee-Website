d90747294911d95119a06c027fd4d77d22e77d39
zach rizzo <zachcilwa@gmail.com>
2022-10-23 18:47:07 -0700

    inital beta build

--- pages/VitalizeBookAnAppointment.tsx
+++ pages/VitalizeBookAnAppointment.tsx
@@ -194,18 +194,18 @@ const vitalizeBookAnAppointment: NextPage<{}> = () => {
                   message: message,
                 })
                 alert('Appointment Booked')
-                // sendMessageFunction({
-                //   message: `${firstName} ${lastName} Booked an appointment from facebook add for ${date} at ${time}. There Phone Number is ${phoneNumber} and their email is ${email}. This is the message they left: ${message}`,
-                //   phone: '+16024482542',
-                // })
-                // sendMessageFunction({
-                //   message: `${firstName} ${lastName} Booked an appointment from facebook add for ${date} at ${time}. There Phone Number is ${phoneNumber} and their email is ${email}. This is the message they left: ${message}`,
-                //   phone: '+14806006604',
-                // })
-                // sendMessageFunction({
-                //   message: `${firstName} ${lastName} Booked an appointment from facebook add for ${date} at ${time}. There Phone Number is ${phoneNumber} and their email is ${email}. This is the message they left: ${message}`,
-                //   phone: '+16233133383',
-                // })
+                sendMessageFunction({
+                  message: `${firstName} ${lastName} Booked an appointment from facebook add for ${date} at ${time}. There Phone Number is ${phoneNumber} and their email is ${email}. This is the message they left: ${message}`,
+                  phone: '+16024482542',
+                })
+                sendMessageFunction({
+                  message: `${firstName} ${lastName} Booked an appointment from facebook add for ${date} at ${time}. There Phone Number is ${phoneNumber} and their email is ${email}. This is the message they left: ${message}`,
+                  phone: '+14806006604',
+                })
+                sendMessageFunction({
+                  message: `${firstName} ${lastName} Booked an appointment from facebook add for ${date} at ${time}. There Phone Number is ${phoneNumber} and their email is ${email}. This is the message they left: ${message}`,
+                  phone: '+16233133383',
+                })
 
                 //claear all fields
                 setFirstName('')
