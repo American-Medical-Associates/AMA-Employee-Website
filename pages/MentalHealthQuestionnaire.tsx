147753b0729e1f9c1f9030a23f3b4cdcf8fbedf0
Mckade Daines <mckadedaines1@gmail.com>
2023-01-23 11:04:24 -0700

    Push test

---
+++ pages/MentalHealthQuestionnaire.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const MentalHealthQuestionnaire: NextPage<{}> = () => {
+  return (
+    <div>
+      Hello this is a test
+    </div>
+  )
+}
+
+export default MentalHealthQuestionnaire
