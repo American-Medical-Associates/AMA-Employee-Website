88da47f7e0bddf6025bad807e83be3330628b852
Mckade Daines <mckadedaines1@gmail.com>
2024-03-26 13:19:49 -0700

    update

--- pages/PhentermineForm.tsx
+++ pages/PhentermineForm.tsx
@@ -6,7 +6,6 @@ import React, {
 } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
 import { auth, submitPhentermineForm } from '../firebase/firebase'
 import router from 'next/router'
 import DateFormatter from './Formatters/DateFormatter'
